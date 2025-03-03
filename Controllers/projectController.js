48f81b42c79674bc020309b0acf59fedb48d084b
batista0523 <elisaulbatista@pursuit.org>
2025-03-03 15:04:19 -0500

    created all CRUD queries

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
-const { getAllProjects, getOneProject } = require("../Queries/project");
+const {
+  getAllProjects,
+  getOneProject,
+  updateProjects,
+  createProject,
+  deleteProject,
+} = require("../Queries/project");
 
 const Project = express.Router();
 
@@ -13,5 +19,37 @@ Project.get("/", async (req, res) => {
     res.status(500).json({ success: false, error: "error fetching internal" });
   }
 });
+
+Project.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const oneProject = await getOneProject(id);
+    if (oneProject) {
+      res.status(200).json({ success: true, payload: oneProject });
+    } else {
+      res.status(404).json({ success: false, error: "Page Not Found CTRL" });
+    }
+  } catch (err) {
+    console.error("Error internal", err);
+    res.status(500).json({ success: false, error: "Catch internal error" });
+  }
+});
+
+Project.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedProject = await updateProjects(id, req.body);
+    if (updatedProject) {
+      res.status(200).json({ success: true, payload: updatedProject });
+    } else {
+      res.status(404).json({ success: false, error: "Page Not Found CTRL" });
+    }
+  } catch (err) {
+    console.error("Internal Error updating the project", err);
+    res.status(500).json({ success: false, error: "Catch Internal error" });
+  }
+});
+
+
 
 module.exports = Project;
--- Queries/project.js
+++ Queries/project.js
@@ -1,4 +1,4 @@
-const db = require("../db/db.Config")
+const db = require("../db/db.Config");
 
 const getAllProjects = async () => {
   try {
@@ -17,13 +17,85 @@ const getOneProject = async (id) => {
     ]);
     return oneProject;
   } catch (err) {
-    console.error("Error fetching one project", err);
+    console.error("Error fetching one project QRY", err);
     throw err;
   }
 };
 
+const updateProjects = async (id, project) => {
+  try {
+    const {
+      title,
+      image,
+      description,
+      production_link,
+      github_repo_back,
+      github_repo_front,
+    } = project;
+    const updatedProject = await db.one(
+      "UPDATE projects SET title=$1, image=$2, description=$3, production_link=$4, github_repo_back=$5, github_repo_front=$6 WHERE id=$7 RETURNING *  ",
+      [
+        title,
+        image,
+        description,
+        production_link,
+        github_repo_back,
+        github_repo_front,
+        id,
+      ]
+    );
+    return updatedProject;
+  } catch (err) {
+    console.error("Error updating project QRY", err);
+    throw err;
+  }
+};
 
-module.exports= {
-getAllProjects,
-getOneProject
-}
+const createProject = async (project) => {
+  try {
+    const {
+      title,
+      image,
+      description,
+      production_link,
+      github_repo_back,
+      github_repo_front,
+    } = project;
+    const createdProject = await db.one(
+      "INSERT INTO projects (title, image, description, production_link, github_repo_back, github_repo_front ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        title,
+        image,
+        description,
+        production_link,
+        github_repo_back,
+        github_repo_front,
+      ]
+    );
+    return createdProject;
+  } catch (err) {
+    console.error("Error creating project QRY", err);
+    throw err;
+  }
+};
+
+const deleteProject = async (id) => {
+  try {
+    const deletedProject = await db.one(
+      "DELETE FROM projects WHERE id=$1 RETURNING *",
+      [id]
+    );
+    return deletedProject;
+  } catch (err) {
+    console.error("Error deleting project QRY", err);
+    throw err;
+  }
+};
+
+module.exports = {
+  getAllProjects,
+  getOneProject,
+  updateProjects,
+  createProject,
+  deleteProject,
+};
